f0b665e02179d1fee34300012237c31973a8c8be
Keon <kroohpar@gmail.com>
2022-11-03 10:48:15 -0700

    Added links to online implementations

--- source/model_architecture.py
+++ source/model_architecture.py
@@ -1,5 +1,7 @@
 """
 This is the file that contains the model implementation for the chess estimator agent. 
+LINK: https://colab.research.google.com/drive/1smI2B7kiwzkr43TqnCYOpxocZlI0kPUh?usp=sharing
+LINK2: https://towardsdatascience.com/train-your-own-chess-ai-66b9ca8d71e4
 
 Author: Keon Roohparvar
 Date: 10/30/22

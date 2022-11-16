8b7f40b7e3cee890884bfcc0e64f55188ee8c5fe
Keon <kroohpar@gmail.com>
2022-11-16 15:13:19 -0800

    New Board implementation

--- source/board.py
+++ source/board.py
@@ -15,7 +15,6 @@ class King():
     def move(self, location):
         print("move the piece")
 
-
 class Queen():
     def __init__(self, name, value, moves_list,  position, color):
         self.name = name
@@ -31,7 +30,6 @@ class Queen():
         print("move the piece")
     def take(self, location):
         print("take a piece")
-
 
 class Bishop():
     def __init__(self, name, value, moves_list,  position, color):
@@ -145,7 +143,6 @@ class Bishop():
     def take(self, location):
         print("take a piece")
 
-
 class Knight():
     def __init__(self, name, value, moves_list,  position, color):
         self.name = name
@@ -162,7 +159,6 @@ class Knight():
     def take(self, location):
         print("take a piece")
 
-
 class Rook():
     def __init__(self, name, value, moves_list,  position, color):
         self.name = name
@@ -178,7 +174,6 @@ class Rook():
         print("move the piece")
     def take(self, location):
         print("take a piece")
-
 
 class Pawn():
     def __init__(self, name, value, moves_list,  position, color):
@@ -262,16 +257,26 @@ class Pawn():
         print("take a piece")
 
 class ChessBoard():
-    def __init__(self, name, state):
+    def __init__(self, name):
         self.name = name
-        self.state = state
+        self.state = [[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(8)]
         self.counter = 0
         self.enc = ''
 
-    def encode_position(self):
-        print("Encode Position")
-        print(self.state)
+        # Calls initialize board to start the board
+        self.init_board()
+    
+    def init_board(self):
+        """
+        Creates initial board implementation.        
+        """
+        # Add Pieces
+        pieces = self.create_all_pieces()
 
+        # Populate board
+        self.populate_board(pieces)
+
+    def encode_position(self):
         for i in range(len(self.state)):
             for j in range(len(self.state[i])):
                 if self.state[i][j]:
@@ -311,89 +316,100 @@ class ChessBoard():
                         self.enc += str(self.counter)
                         self.enc += '/'
                         self.counter = 0
-        print(self.enc)
-
-def create_all_pieces():  # this function will create all the pieces in the game, and
-    all_pieces = []
-    # all minor and major black pieces
-    BR1 = Rook("BlackRook1", 5, ["up", "down", "left", "right"], [0, 0], 2)
-    BR2 = Rook("BlackRook2", 5, ["up", "down", "left", "right"], [0, 7], 2)
-    BN1 = Knight("BlackKnight1", 3, ["makes an L"], [0, 1], 2)
-    BN2 = Knight("BlackKnight2", 3, ["makes an L"], [0, 6], 2)
-    BB1 = Bishop("BlackBishop1", 3, ["digaonals"], [0, 2], 2)
-    BB2 = Bishop("BlackBishop2", 3, ["digaonals"], [0, 5], 2)
-    BQ = Queen("BlackQueen", 9, ["up", "down", "left", "right", "diagonals"], [0, 3], 2)
-    BK = King("BlackKing", 100, ["up", "down", "left", "right", "diagonals"], [0, 4], 2)
-
-    # all black pawns
-    BP1 = Pawn("BlackPawn1", 1, ["up", "diagonal"], [1, 0], 2)
-    BP2 = Pawn("BlackPawn2", 1, ["up", "diagonal"], [1, 1], 2)
-    BP3 = Pawn("BlackPawn3", 1, ["up", "diagonal"], [1, 2], 2)
-    BP4 = Pawn("BlackPawn4", 1, ["up", "diagonal"], [1, 3], 2)
-    BP5 = Pawn("BlackPawn5", 1, ["up", "diagonal"], [1, 4], 2)
-    BP6 = Pawn("BlackPawn6", 1, ["up", "diagonal"], [1, 5], 2)
-    BP7 = Pawn("BlackPawn7", 1, ["up", "diagonal"], [1, 6], 2)
-    BP8 = Pawn("BlackPawn8", 1, ["up", "diagonal"], [1, 7], 2)
-
-    black_pieces = [BR1, BR2, BN1, BN2, BB1, BB2, BQ, BK, BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8]
-    all_pieces.extend(black_pieces)
-
-    # all minor and major white pieces
-    WR1 = Rook("WhiteRook1", 5, ["up", "down", "left", "right"], [7, 0], 1)
-    WR2 = Rook("WhiteRook2", 5, ["up", "down", "left", "right"], [7, 7], 1)
-    WN1 = Knight("WhiteKnight1", 3, ["makes an L"], [7, 1], 1)
-    WN2 = Knight("WhiteKnight2", 3, ["makes an L"], [7, 6], 1)
-    WB1 = Bishop("WhiteBishop1", 3, ["digaonals"], [7, 2], 1)
-    WB2 = Bishop("WhiteBishop2", 3, ["digaonals"], [7, 5], 1)
-    WQ = Queen("WhiteQueen", 9, ["up", "down", "left", "right", "diagonals"], [7, 3], 1)
-    WK = King("WhiteKing", 100, ["up", "down", "left", "right", "diagonals"], [7, 4], 1)
-
-    # all white pawns
-    WP1 = Pawn("WhitePawn1", 1, ["up", "diagonal"], [6, 0], 1)
-    WP2 = Pawn("WhitePawn2", 1, ["up", "diagonal"], [6, 1], 1)
-    WP3 = Pawn("WhitePawn3", 1, ["up", "diagonal"], [6, 2], 1)
-    WP4 = Pawn("WhitePawn4", 1, ["up", "diagonal"], [6, 3], 1)
-    WP5 = Pawn("WhitePawn5", 1, ["up", "diagonal"], [6, 4], 1)
-    WP6 = Pawn("WhitePawn6", 1, ["up", "diagonal"], [6, 5], 1)
-    WP7 = Pawn("WhitePawn7", 1, ["up", "diagonal"], [6, 6], 1)
-    WP8 = Pawn("WhitePawn8", 1, ["up", "diagonal"], [6, 7], 1)
-
-    white_pieces = [WR1, WR2, WN1, WN2, WB1, WB2, WQ, WK, WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8]
-    all_pieces.extend(white_pieces)
-    return all_pieces
-
-def create_board():
-    og_board = [[0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0]]
-    CB1 = ChessBoard("test_board", og_board)
-    return CB1
-
-def populate_board(all_pieces, board):
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            for itr in range(len(all_pieces)):
-                if all_pieces[itr].position == [i, j]:
-                    board[i][j] = all_pieces[itr]
-
-    for i in range(len(board)):
-        print(board[i])
+        return self.enc
     
-    print(board)
+    def create_all_pieces(self):  
+        all_pieces = []
+        # all minor and major black pieces
+        BR1 = Rook("BlackRook1", 5, ["up", "down", "left", "right"], [0, 0], 2)
+        BR2 = Rook("BlackRook2", 5, ["up", "down", "left", "right"], [0, 7], 2)
+        BN1 = Knight("BlackKnight1", 3, ["makes an L"], [0, 1], 2)
+        BN2 = Knight("BlackKnight2", 3, ["makes an L"], [0, 6], 2)
+        BB1 = Bishop("BlackBishop1", 3, ["digaonals"], [0, 2], 2)
+        BB2 = Bishop("BlackBishop2", 3, ["digaonals"], [0, 5], 2)
+        BQ = Queen("BlackQueen", 9, ["up", "down", "left", "right", "diagonals"], [0, 3], 2)
+        BK = King("BlackKing", 100, ["up", "down", "left", "right", "diagonals"], [0, 4], 2)
+
+        # all black pawns
+        BP1 = Pawn("BlackPawn1", 1, ["up", "diagonal"], [1, 0], 2)
+        BP2 = Pawn("BlackPawn2", 1, ["up", "diagonal"], [1, 1], 2)
+        BP3 = Pawn("BlackPawn3", 1, ["up", "diagonal"], [1, 2], 2)
+        BP4 = Pawn("BlackPawn4", 1, ["up", "diagonal"], [1, 3], 2)
+        BP5 = Pawn("BlackPawn5", 1, ["up", "diagonal"], [1, 4], 2)
+        BP6 = Pawn("BlackPawn6", 1, ["up", "diagonal"], [1, 5], 2)
+        BP7 = Pawn("BlackPawn7", 1, ["up", "diagonal"], [1, 6], 2)
+        BP8 = Pawn("BlackPawn8", 1, ["up", "diagonal"], [1, 7], 2)
+
+        black_pieces = [BR1, BR2, BN1, BN2, BB1, BB2, BQ, BK, BP1, BP2, BP3, BP4, BP5, BP6, BP7, BP8]
+        all_pieces.extend(black_pieces)
+
+        # all minor and major white pieces
+        WR1 = Rook("WhiteRook1", 5, ["up", "down", "left", "right"], [7, 0], 1)
+        WR2 = Rook("WhiteRook2", 5, ["up", "down", "left", "right"], [7, 7], 1)
+        WN1 = Knight("WhiteKnight1", 3, ["makes an L"], [7, 1], 1)
+        WN2 = Knight("WhiteKnight2", 3, ["makes an L"], [7, 6], 1)
+        WB1 = Bishop("WhiteBishop1", 3, ["digaonals"], [7, 2], 1)
+        WB2 = Bishop("WhiteBishop2", 3, ["digaonals"], [7, 5], 1)
+        WQ = Queen("WhiteQueen", 9, ["up", "down", "left", "right", "diagonals"], [7, 3], 1)
+        WK = King("WhiteKing", 100, ["up", "down", "left", "right", "diagonals"], [7, 4], 1)
+
+        # all white pawns
+        WP1 = Pawn("WhitePawn1", 1, ["up", "diagonal"], [6, 0], 1)
+        WP2 = Pawn("WhitePawn2", 1, ["up", "diagonal"], [6, 1], 1)
+        WP3 = Pawn("WhitePawn3", 1, ["up", "diagonal"], [6, 2], 1)
+        WP4 = Pawn("WhitePawn4", 1, ["up", "diagonal"], [6, 3], 1)
+        WP5 = Pawn("WhitePawn5", 1, ["up", "diagonal"], [6, 4], 1)
+        WP6 = Pawn("WhitePawn6", 1, ["up", "diagonal"], [6, 5], 1)
+        WP7 = Pawn("WhitePawn7", 1, ["up", "diagonal"], [6, 6], 1)
+        WP8 = Pawn("WhitePawn8", 1, ["up", "diagonal"], [6, 7], 1)
+
+        white_pieces = [WR1, WR2, WN1, WN2, WB1, WB2, WQ, WK, WP1, WP2, WP3, WP4, WP5, WP6, WP7, WP8]
+        all_pieces.extend(white_pieces)
+        return all_pieces
+    
+    def populate_board(self, all_pieces):
+        for i in range(len(self.state)):
+            for j in range(len(self.state[i])):
+                for itr in range(len(all_pieces)):
+                    if all_pieces[itr].position == [i, j]:
+                        self.state[i][j] = all_pieces[itr]
+
+    def print_board(self):
+        print('Printing board...')
+        for i in range(len(self.state)):
+            print(self.state[i])
+    
+    def make_move(self, move):
+        player_to_move = 'w' if (self.counter % 2 == 0) else 'b'
+        
+        # White Move
+        if player_to_move == 'w':
+            pass
+    
+    def find_piece_to_move(self, move):
+        piece_char = move[0]
+
+        # Pawn
+        if piece_char == piece_char.lower():
+            # Iterate over all pawns and find the one that can move
+            pass
 
 
-all_pieces = create_all_pieces()
-playing_board = create_board()
-populate_board(all_pieces, playing_board.state)
+        
+if __name__ == '__main__':
+    board = ChessBoard('Test Board')
+    print(board.encode_position())
 
 
-possible_moves = playing_board.state[1][3].find_moves(playing_board.state)
-print(possible_moves)
+    """ BELOW USED BEFORE FOR TESTING
+    all_pieces = create_all_pieces()
+    playing_board = create_board()
+    populate_board(all_pieces, playing_board.state)
 
-board = ChessBoard('Coreys Board', playing_board.state)
-board.encode_position()
+
+    possible_moves = playing_board.state[1][3].find_moves(playing_board.state)
+    print(possible_moves)
+
+    board = ChessBoard('Coreys Board', playing_board.state)
+    board.encode_position()
+    """
---
+++ source/new_board.py
@@ -0,0 +1,71 @@
+"""
+This is NOT our implementation of a chess board, but rather using the Python API 'Chess'.
+"""
+
+import chess
+
+class ChessBoard:
+    def __init__(self):
+        self.board = chess.Board()
+        self.piece_dict = {
+            'r': 1,
+            'n': 2,
+            'b': 3,
+            'q': 4,
+            'k': 5,
+            'p': 6,
+            'R': 7,
+            'N': 8,
+            'B': 9,
+            'Q': 10,
+            'K': 11,
+            'P': 12
+        }
+    
+    def make_move(self, move):
+        if chess.Move.from_uci(move) not in self.board.legal_moves:
+            print('ERROR - Illegal Move.')
+            return -1
+        
+        self.board.push_san(move)
+        return 0
+    
+    def positional_encode(self):
+        print('Board position is: ' + self.board.fen())
+        # Parse the fen notation 
+        fen = self.board.fen()
+
+        pieces, move, castles, en_passant, halfmove_clock, fullmove_num = fen.split(' ')
+
+        # Parse pieces
+        pieces_arr = []        
+        rows = pieces.split('/')
+        for row in rows:
+            for spot in row:
+                if spot.isdigit():
+                    pieces_arr.extend([0 for _ in range(int(spot))])
+                else:
+                    pieces_arr.append(self.piece_dict[spot])
+        
+        # Parse Move
+        move_arr = [1] if move == 'w' else [0]
+
+        # Parse castles
+        castles_arr = ['K' in castles, 'Q' in castles, 'k' in castles, 'q' in castles]
+        for idx, bool in enumerate(castles_arr):
+            if bool:
+                castles_arr[idx] = 1
+            else:
+                castles_arr[idx] = 0
+        
+        # Parse en passant
+        en_passant_arr = [0] if en_passant == '-' else [chess.parse_square(en_passant)]
+
+        return pieces_arr + move_arr + castles_arr + move_arr + en_passant_arr + [int(halfmove_clock)] + [int(fullmove_num)]
+
+
+    
+
+if __name__ == '__main__':
+    board = ChessBoard()
+    print(board.positional_encode())

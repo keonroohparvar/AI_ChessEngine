f4ae61c2b807f7971c318d072406e28c32e6a5e2
SammySmash613 <36828580+SammySmash613@users.noreply.github.com>
2023-04-19 20:58:59 -0700

    created sammy file

--- source/monte_carlo_sammy.py
+++ source/monte_carlo_sammy.py
@@ -1,5 +1,5 @@
 """
-This script will be the Monte Carlo implementation that will be the way our Chess Engine is able
+Monte Carlo implementation that will be the way our Chess Engine is able
 to go down potential moves and choose the best one.
 
 Author: Sammy Paykel
